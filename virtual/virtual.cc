f6108a03e9e0caf660a692fe1430b67f2db16a93
vijayanandhini <vijayanandhini.mano@gmail.com>
2016-10-16 18:38:10 +0530

    1st commit

---
+++ virtual/virtual.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<unistd.h>
+#include<thread>
+#include<chrono>
+#include<iomanip>
+
+using namespace std::this_thread;
+using namespace std::chrono;
+
+class twinkle 
+{
+  public:
+    void execute()
+    {
+     std::cout<<"\n in twinkle function \n";
+     }
+};
+
+class factory
+{
+public :
+void run()
+{
+  std::cout<<"\n in run function \n";
+ auto *cmd=new twinkle();
+   cmd->execute();
+}
+};
+
+
+
+
+
+
+int main()
+{
+  int min=1,wait,sec=55;
+  factory shell;
+  std::cout<<"\n in main \n";
+  shell.run();
+  return 0;
+}
